e1eb183cf95b1ee7185254a1ef6e18e02ba33b09
Nishant Manjunath <nishant.manjunath@NISHANT-M.local>
2026-02-23 13:26:34 +0530

    Add root route to fix empty page on deployment

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<PageWidgetBuilder />} />
         <Route path="/widget-builder" element={<PageWidgetBuilder />} />
       </Routes>
     </BrowserRouter>
